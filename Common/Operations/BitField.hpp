1d70e47879b8936d7ddfafbd671bbeb6ef5c4fc6
m.sarmad <m.sarmad@noose.info>
2026-08-23 10:06:31 +0330

    [ADD] Adding BitField

--- Common/Operations/BitField.hpp
+++ Common/Operations/BitField.hpp
@@ -2,20 +2,46 @@
 #define SURE_COMMON_BASE_OPERATIONS_BITFIELD_HPP_
 
 #include "../Base/Lang.hpp"
+#include "../Types/Primitives/Inc.hpp"
 #include "../Types/Primitives/Size.hpp"
+
 
 namespace sure::base {
 
-    template<typename T, T OFFSET, T WIDTH>
+    template<typename T, U8 OFFSET, U8 WIDTH>
     class BitField {
         static_assert(OFFSET + WIDTH <= sizeof(T) * CHAR_SIZE_);
 
+      public:
         static CONSTEXPR_ T mask() noexcept {
-            // return
+            if constexpr (sizeof(T) == sizeof(U64) ) {
+                return ~static_cast<T>(0);
+            }
+            else {
+                return static_cast<T>(1) << WIDTH - 1;
+            }
         }
 
         static T write(T reg, T value) noexcept {
+            reg |= BitField::mask();
+            value &= BitField::mask();
+            return reg | value;
+        }
 
+        static T extract(T reg) noexcept {
+            return reg & BitField::mask();
+        }
+
+        static Bool is_enabled(T reg) noexcept {
+            return (reg & BitField::mask()) == 1;
+        }
+
+        static Bool is_disabled(T reg) noexcept {
+            return (reg & BitField::mask()) == 0;
+        }
+
+        static Bool is_valid(T value) noexcept {
+            return (value | BitField::mask()) == BitField::mask();
         }
     };
 
